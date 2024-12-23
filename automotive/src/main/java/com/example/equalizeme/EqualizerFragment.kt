6211a43dab380235948d32e81d26c1521ce78540
Amanda Briena <55834808+amandabriena@users.noreply.github.com>
2024-12-23 01:52:30 -0300

    Added VM in EqualizerFragment

--- automotive/src/main/java/com/example/equalizeme/EqualizerFragment.kt
+++ automotive/src/main/java/com/example/equalizeme/EqualizerFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import com.example.equalizeme.viewmodel.MainViewModel
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,6 +22,7 @@ class EqualizerFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private lateinit var mViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +36,9 @@ class EqualizerFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+        mViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_equalizer, container, false)
     }

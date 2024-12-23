932c9cecfff943b34c62869106e67d2ba668c347
Amanda Briena <55834808+amandabriena@users.noreply.github.com>
2024-12-23 01:53:00 -0300

    Added VM in ProfileFragment

--- automotive/src/main/java/com/example/equalizeme/ProfileFragment.kt
+++ automotive/src/main/java/com/example/equalizeme/ProfileFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.equalizeme.databinding.FragmentProfileBinding
 import com.example.equalizeme.databinding.ItemPerfilBinding
+import com.example.equalizeme.model.Equalizer
+import com.example.equalizeme.model.Profile
+import com.example.equalizeme.viewmodel.MainViewModel
 
 
 class ProfileFragment : Fragment() {
@@ -16,6 +20,7 @@ class ProfileFragment : Fragment() {
     private lateinit var perfilItem1: ItemPerfilBinding
     private lateinit var perfilItem2: ItemPerfilBinding
     private lateinit var perfilItem3: ItemPerfilBinding
+    private lateinit var mViewModel: MainViewModel
 
     private var _binding: FragmentProfileBinding? = null
     // This property is only valid between onCreateView and
@@ -29,14 +34,22 @@ class ProfileFragment : Fragment() {
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val view = binding.root
 
+        mViewModel = ViewModelProvider(requireActivity())[MainViewModel::class.java]
+
         perfilItem1 = binding.profileItem1
         perfilItem1.name.text = "Profile 1"
         perfilItem1.icon.visibility = View.GONE
 
+        val profile1 = Profile(1, "Profile 1", Equalizer())
+        mViewModel.addProfile(profile1)
+
         perfilItem2 = binding.profileItem2
         binding.addButton.root.setOnClickListener { adicionarPerfil() }
 
-        binding.profileItem1.root.setOnClickListener { findNavController().navigate(R.id.action_firstFragment_to_secondFragment) }
+        binding.profileItem1.root.setOnClickListener {
+            mViewModel.selectProfile(profile1)
+            findNavController().navigate(R.id.action_firstFragment_to_secondFragment)
+        }
 
         return view
     }
@@ -48,6 +61,9 @@ class ProfileFragment : Fragment() {
                 perfilItem2.root.visibility = View.VISIBLE
                 perfilItem2.name.text = "Profile 2"
                 perfilItem2.icon.visibility = View.GONE
+
+                val profile2 = Profile(2, "Profile 2", Equalizer())
+                mViewModel.addProfile(profile2)
             }
             2 -> {
                 //val addButtonTextView = addButton.findViewById<TextView>(R.id.text_nome_perfil)
@@ -56,6 +72,9 @@ class ProfileFragment : Fragment() {
                 binding.addButton.root.visibility = View.VISIBLE
                 //addButton.findViewById<ImageView>(R.id.addIcon).visibility = View.GONE
                 binding.addButton.root.setOnClickListener(null) // Remove o listener
+
+                val profile3 = Profile(3, "Profile 3", Equalizer())
+                mViewModel.addProfile(profile3)
             }
         }
     }

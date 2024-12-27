9ea427f0fd15f4aaa6caab8d09bd6a20fdb1ea9e
Amanda Briena <55834808+amandabriena@users.noreply.github.com>
2024-12-27 00:13:25 -0300

    Adjust unit tests

--- automotive/src/test/java/com/example/equalizeme/viewmodel/MainViewModelTest.kt
+++ automotive/src/test/java/com/example/equalizeme/viewmodel/MainViewModelTest.kt
@@ -1,24 +1,51 @@
 package com.example.equalizeme.viewmodel
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import com.example.equalizeme.model.Equalizer
-import com.example.equalizeme.model.Profile
+import android.app.Application
+import android.os.Build
+import com.example.equalizeme.model.UserProfile
+import com.example.equalizeme.services.profiles.UserProfileRepository
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.StandardTestDispatcher
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.robolectric.annotation.Config
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.robolectric.util.ReflectionHelpers
 
+@Config(sdk = [Build.VERSION_CODES.R])
 @ExperimentalCoroutinesApi
 class MainViewModelTest {
     @get:Rule
     val instantTaskExecutorRule = InstantTaskExecutorRule()
 
     private lateinit var SUT: MainViewModel
+    @MockK
+    private lateinit var mockRepository: UserProfileRepository
+    @MockK
+    private lateinit var mockApplication: Application
+
+    private val testDispatcher = StandardTestDispatcher()
 
     @Before
     fun setUp() {
-        SUT = MainViewModel()
+        MockKAnnotations.init(this)
+        every { mockApplication.applicationContext } returns mockApplication
+        coEvery { mockRepository.userProfilesFlow } returns flowOf(emptyList())
+        Dispatchers.setMain(testDispatcher)
+
+        SUT = MainViewModel(mockApplication)
     }
 
     @Test
@@ -30,55 +57,48 @@ class MainViewModelTest {
     }
 
     @Test
-    fun `check profile added`() {
-        //Arrange
-        val id = 10
-        val newProfile = Profile(id, "testName", Equalizer())
+    fun `addProfile does nothing when profiles are null`() = runTest {
+        // Arrange
+        val profiles = listOf(UserProfile.newBuilder().setName("Profile 1").build())
+        coEvery { mockRepository.userProfilesFlow } returns flowOf(profiles)
+        coEvery { mockRepository.createProfile(any()) } returns Unit
+        SUT.profilesFlow = flowOf(profiles)
+        SUT.repository = mockRepository
 
-        //Act
-        SUT.addProfile(newProfile)
+        // Act
+        SUT.addProfile()
+        testScheduler.runCurrent()
 
-        //Assert
-        assertFalse(SUT.profiles.value.isNullOrEmpty())
-        assertEquals(SUT.profiles.value?.get(0)?.id, id)
+        // Assert
+        // Assert
+        coVerify { mockRepository.createProfile(any()) }
     }
 
     @Test
-    fun `check profile selected correctly`() {
-        //Arrange
-        val id = 1
-        val eq = 10
-        val equalizer = Equalizer(eq, eq, eq)
-        val newProfile = Profile(id, "testName", equalizer)
-        SUT.addProfile(newProfile)
+    fun `test addProfile does not add a profile when 3 profiles exist`() = runTest {
+        // Arrange
+        val profiles = List(3) { UserProfile.newBuilder().setName("Profile ${it + 1}").build() }
+        coEvery { mockRepository.userProfilesFlow } returns flowOf(profiles)
+        coEvery { mockRepository.createProfile(any()) } returns Unit
+        SUT.profilesFlow = flowOf(profiles)
+        SUT.repository = mockRepository
 
-        //Act
-        SUT.selectProfile(newProfile)
+        // Act
+        SUT.addProfile()
+        testScheduler.runCurrent()
 
-        //Assert
-        assertEquals(eq, SUT.currentEqualizer.value?.mBass)
-        assertEquals(eq, SUT.currentEqualizer.value?.mMid)
-        assertEquals(eq, SUT.currentEqualizer.value?.mTreble)
+        // Assert
+        coVerify(exactly = 0) { mockRepository.createProfile(any()) }
     }
 
     @Test
-    fun `check update equalizer`() {
-        //Arrange
-        val id = 1
-        val newEq = 7
-        val equalizer = Equalizer(10, 10, 10)
-        val newProfile = Profile(id, "testName", equalizer)
-        SUT.addProfile(newProfile)
+    fun `test selectProfile updates currentProfileIndexFlow`() = runTest {
+        // Act
+        SUT.selectProfile(1)
+        val currentProfileIndexFlow: MutableStateFlow<*> = ReflectionHelpers.getField(SUT, "currentProfileIndexFlow") as MutableStateFlow<*>
 
-        val newEqualizer = Equalizer(newEq, newEq, newEq)
-
-        //Act
-        SUT.updateEqualizer(newEqualizer)
-
-        //Assert
-        assertEquals(newEq, SUT.currentEqualizer.value?.mBass)
-        assertEquals(newEq, SUT.currentEqualizer.value?.mMid)
-        assertEquals(newEq, SUT.currentEqualizer.value?.mTreble)
+        // Assert
+        assertEquals(1, currentProfileIndexFlow.value)
     }
 
 }
